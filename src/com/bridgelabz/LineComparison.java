8c654affff74a2684f8865ad5ecd9c4173409423
Kisalay <kisalaykisu@gmail.com>
2022-06-18 11:19:01 +0530

    first commit

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class LineComparison {
+    public static void main(String args[]) {
+        //Welcome Message.
+        System.out.println("Welcome into the playground of coding,Line comparison computation is the first one.");
+    }
+}
