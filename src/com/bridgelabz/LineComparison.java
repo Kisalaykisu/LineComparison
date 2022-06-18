096ee6e9b2214533467f0d1fe19f24123cfd8fb9
Kisalay <kisalaykisu@gmail.com>
2022-06-18 11:43:53 +0530

    file has been updated:

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,12 +1,7 @@
 /*
-        Case 2 :- As a fan of geometry, I want to
-        check equality of two lines
-        based on the end points, So
-        that I know when two lines are
-        the equal.
+   Case 3:-  Calculation of length of two lines: line1 Points (x1, y1) and (x2, y2) and
+             line2 Points (x4, y4) and (x4, y4).
 
-        a) Using Java equals method to check equality of 2 Lengths is
-        preferable.
  */
 
 
@@ -15,33 +10,37 @@
 package com.bridgelabz;
 import java.util.Scanner;   // import scanner
 
+import static java.lang.Double.compare;
 public class LineComparison {
 
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in); // Make scanner obj
+        Scanner scanner = new Scanner(System.in);  //  Make scanner obj
         float x1,y1,x2,y2;
 
         System.out.println("Enter x1 y1 x2 y2 of line one: ");
-        x1 = scanner.nextFloat();  //Float input
-        y1 = scanner.nextFloat();
+        x1 = scanner.nextFloat();
+        y1 = scanner.nextFloat();  // float input
         x2 = scanner.nextFloat();
         y2 = scanner.nextFloat();
-
-        double length1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );  // Mathematical Formula
+        double l1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );   // Mathematical formula
 
         System.out.println("Enter x1 y1 x2 y2 of line two: ");
         x1 = scanner.nextFloat();
         y1 = scanner.nextFloat();
         x2 = scanner.nextFloat();
         y2 = scanner.nextFloat();
+        double l2 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
 
-        double length2 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+        int x = compare(l1, l2);
+        System.out.println(x);
 
-        if (length1==length2){
+        if (x==0){
             System.out.println("Both the lines are equal in length. ");
-        }else
-            System.out.println("Both the lines are not equal in length. ");
+        }else if (x<0)
+            System.out.println("line one is smaller than line two. ");
+        else
+            System.out.println("line one is larger than line two. ");
     }
-    }
+}
 
