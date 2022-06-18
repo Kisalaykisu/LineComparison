0165bf1b04900768cb9aff2b337f53fcf1081d66
Kisalay <kisalaykisu@gmail.com>
2022-06-18 11:33:07 +0530

    File has been updated:

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,9 +1,13 @@
 /*
-  Case 1 :-  As a fan of geometry, I want to model a line based on a point
-             consisting of (x, y) co-ordinates using the Cartesian system,
-             So that I can calculate its length..
+        Case 2 :- As a fan of geometry, I want to
+        check equality of two lines
+        based on the end points, So
+        that I know when two lines are
+        the equal.
 
-*/
+        a) Using Java equals method to check equality of 2 Lengths is
+        preferable.
+ */
 
 
 
@@ -15,17 +19,29 @@ public class LineComparison {
 
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);  // Make scanner obj
+        Scanner scanner = new Scanner(System.in); // Make scanner obj
         float x1,y1,x2,y2;
 
-        System.out.println("Enter x1 y1 x2 y2: ");
-        x1 = scanner.nextFloat();
-        y1 = scanner.nextFloat();  //Float input
+        System.out.println("Enter x1 y1 x2 y2 of line one: ");
+        x1 = scanner.nextFloat();  //Float input
+        y1 = scanner.nextFloat();
         x2 = scanner.nextFloat();
         y2 = scanner.nextFloat();
 
-        double length = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );    // Mathematical used
-        System.out.println(length);
+        double length1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );  // Mathematical Formula
 
+        System.out.println("Enter x1 y1 x2 y2 of line two: ");
+        x1 = scanner.nextFloat();
+        y1 = scanner.nextFloat();
+        x2 = scanner.nextFloat();
+        y2 = scanner.nextFloat();
+
+        double length2 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+
+        if (length1==length2){
+            System.out.println("Both the lines are equal in length. ");
+        }else
+            System.out.println("Both the lines are not equal in length. ");
     }
-}
+    }
+
