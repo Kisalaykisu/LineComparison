d65b30a1cb323f55b109929896a450b5a09e6db0
Kisalay <kisalaykisu@gmail.com>
2022-06-23 18:18:53 +0530

    UC-4 has been added

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,10 +1,44 @@
-
+/* Q.4 ) Use Java Object Oriented Programming Concepts of Line and Point as well as equals and
+      compareTo methods. -
+      Using Java compareTo method to compare 2 Lengths is preferable.*/
 package com.bridgelabz;
-public class LineComparison {
 
-    public static void main(String args[]) {
-        //Welcome Message.
-        System.out.println("Welcome into the playground of coding,Line comparison computation is the first one.");
+import java.util.Scanner;  // import scanner
+import static java.lang.Double.compare;   // method compares the two specified double value
+
+public class LineComparison{
+    public static void main(String[] args){  // Entry Point of Program
+
+        lineComparison();
+
+    }
+    public static void lineComparison(){
+        Scanner scanner = new Scanner(System.in);  // Make Scanner Obj
+        float x1,y1,x2,y2;
+
+        System.out.println("Enter x1 y1 x2 y2 of line one: ");
+        x1 = scanner.nextFloat();  // Input float
+        y1 = scanner.nextFloat();  // Input float
+        x2 = scanner.nextFloat();  // Input float
+        y2 = scanner.nextFloat();  // Input float
+        double l1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );  //Mathematical Formula
+
+        System.out.println("Enter x1 y1 x2 y2 of line two: ");
+        x1 = scanner.nextFloat();  // Input float
+        y1 = scanner.nextFloat();  // Input float
+        x2 = scanner.nextFloat();  // Input float
+        y2 = scanner.nextFloat();
+        double l2 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+
+        int x = compare(l1, l2);  // Compare the two lines
+        System.out.println(x);
+
+        if (x==0){
+            System.out.println("Both the lines are equal in length. ");
+        }else if (x<0)
+            System.out.println("line one is smaller than line two. ");
+        else
+            System.out.println("line one is larger than line two. ");
     }
 }
 
